0a2debf5923694576f4614bfb5c0a487bbbab92b
meetyyj <meetyyj@gmail.com>
2017-06-04 19:49:13 +0800

    add zsh sogoupinyin chrome

---
+++ UbuntuInstall/CLionInstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#intall clion
+mkdir $HOME/Software
+cd $HOME/Software
+wget "https://download.jetbrains.8686c.com/cpp/CLion-2017.1.2.tar.gz"
+tar -xvzf CLion*
+rm -f CLion*.tar.gz
+
--- UbuntuInstall/ubuntuInstall.sh
+++ UbuntuInstall/ubuntuInstall.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-#change /etc/apt/sources.list
-#---------------------------------
-#cp -f sources.list /etc/apt/
+# change /etc/apt/sources.list
+cp -f sources.list /etc/apt/
 
-#intall some package
-#git vim
-#sudo apt install git
-#sudo apt install vim
-#---------------------------------
+# intall some package
+# git vim
+sudo apt -y install git
+sudo apt -y install vim
 
-#install shadowsocks-qt5
-read -p "Do you want to install shadowsocks-qt5?(y/n)" choice
-case $choice in
+# install shadowsocks-qt5
+read -p "Do you want to install shadowsocks-qt5?(y/n)" choice_ssqt5
+case $choice_ssqt5 in
   y|Y|yes|Yes|YES)
   echo "Start installing shadowsocks-qt5:"
   sudo add-apt-repository ppa:hzwhuang/ss-qt5
   sudo apt-get update
-  sudo apt install shadowsocks-qt5
+  sudo apt -y install shadowsocks-qt5
   echo "export http_proxy=http://127.0.0.1:1080" >> $HOME/.bashrc
   echo "export https_proxy=\$http_proxy" >> $HOME/.bashrc
   ;;
@@ -24,13 +22,52 @@ case $choice in
   echo "shadowsocks-qt5 will NOT be installed"
   ;;
 esac
-#install chrome
-#cd $HOME/Downloads
-#wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-#sudo dpkg -i google-chrome-stable*
-#sudo apt -f install
 
-#dowland switchyOmega
-#wget "https://github.com/FelisCatus/SwitchyOmega/releases/download/v2.4.6/SwitchyOmega.crx"
-#wget "https://github.com/FelisCatus/SwitchyOmega/wiki/GFWList.bak"
 
+# install chrome
+read -p "Do you want to install chrome?(y/n)" choice_chrome
+case $choice_chrome in
+  y|Y|yes|Yes|YES)
+  echo "Start installing chrome:"
+  cd $HOME/Downloads
+  wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+  sudo dpkg -i google-chrome-stable*
+  sudo apt -f install
+  # dowland switchyOmega
+  wget "https://github.com/FelisCatus/SwitchyOmega/releases/download/v2.4.6/SwitchyOmega.crx"
+  wget "https://github.com/FelisCatus/SwitchyOmega/wiki/GFWList.bak"
+  ;;
+  *)
+  echo "chrome will NOT be installed"
+  ;;
+esac
+
+# install sogoupinyin
+read -p "Do you want to install sogoupinyin?(y/n)" choice_sogou
+case $choice_sogou in
+  y|Y|yes|Yes|YES)
+  echo "Start installing sogoupinyin:"
+  cd $HOME/Downloads
+  wget "http://cdn2.ime.sogou.com/dl/index/1491565850/sogoupinyin_2.1.0.0086_amd64.deb?st=IA49qx39cM3QM0eU1GHaQA&e=1496571442&fn=sogoupinyin_2.1.0.0086_amd64.deb"
+  sudo apt -y install fcitx
+  sudo dpkg -i sogoupinyin*.deb
+  sudo apt -f install
+  ;;
+  *)
+  echo "sogoupinyin will NOT be installed"
+  ;;
+esac
+
+
+# install zsh
+read -p "Do you want to install zsh?(y/n)" choice_zsh
+case $choice_zsh in
+  y|Y|yes|Yes|YES)
+  echo "Start installing zsh:"
+  sudo apt -y install zsh
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+  ;;
+  *)
+  echo "zsh will NOT be installed"
+  ;;
+esac
