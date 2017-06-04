f217e9c1c43b48360cdb50761af4fd58c9cf485b
meetyyj <meetyyj@gmail.com>
2017-06-04 17:18:24 +0800

    add choice

--- UbuntuInstall/ubuntuInstall.sh
+++ UbuntuInstall/ubuntuInstall.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 #change /etc/apt/sources.list
-cp -f sources.list /etc/apt/
+#---------------------------------
+#cp -f sources.list /etc/apt/
 
 #intall some package
 #git vim
-sudo apt install git
-sudo apt install vim
+#sudo apt install git
+#sudo apt install vim
+#---------------------------------
 
 #install shadowsocks-qt5
-sudo add-apt-repository ppa:hzwhuang/ss-qt5
-sudo apt-get update
-sudo apt install shadowsocks-qt5
-
+read -p "Do you want to install shadowsocks-qt5?(y/n)" choice
+case $choice in
+  y|Y|yes|Yes|YES)
+  echo "Start installing shadowsocks-qt5:"
+  sudo add-apt-repository ppa:hzwhuang/ss-qt5
+  sudo apt-get update
+  sudo apt install shadowsocks-qt5
+  echo "export http_proxy=http://127.0.0.1:1080" >> $HOME/.bashrc
+  echo "export https_proxy=\$http_proxy" >> $HOME/.bashrc
+  ;;
+  *)
+  echo "shadowsocks-qt5 will NOT be installed"
+  ;;
+esac
 #install chrome
-cd $HOME/Downloads
-wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-sudo dpkg -i google-chrome-stable*
-sudo apt -f install
+#cd $HOME/Downloads
+#wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+#sudo dpkg -i google-chrome-stable*
+#sudo apt -f install
 
 #dowland switchyOmega
-wget "https://github.com/FelisCatus/SwitchyOmega/releases/download/v2.4.6/SwitchyOmega.crx"
-wget "https://github.com/FelisCatus/SwitchyOmega/wiki/GFWList.bak"
+#wget "https://github.com/FelisCatus/SwitchyOmega/releases/download/v2.4.6/SwitchyOmega.crx"
+#wget "https://github.com/FelisCatus/SwitchyOmega/wiki/GFWList.bak"
 
---
+++ rosInstall/rosInstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#setup sources.list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+#setup keys
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+#install
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full
