2a20069156874a7254814a42ebb66b9a0a48d950
meetyyj <meetyyj@gmail.com>
2018-01-03 10:27:52 +0800

    add git and fix bugs

--- MasterProject/install.sh
+++ MasterProject/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+cur_dir=$PWD
 sudo apt update
 
 #install vim
 sudo apt install -y vim
-
+sudo apt install -y git
 #install mjpg_streamer
 sudo apt install -y cmake libjpeg8-dev
 mkdir $HOME/Software
@@ -15,5 +16,7 @@ cd mjpg-streamer-master/mjpg-streamer-experimental
 make
 sudo make install
 cd /etc/init.d
-cp $HOME/wifirobot.sh .
+cp $PWD/wifirobot.sh .
 sudo update-rc.d wifirobot.sh defaults 95
+
+git clone https://github.com/MeetYyj/Robot306.git
